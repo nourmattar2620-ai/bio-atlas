172f215ef40e9b364023ca8814b08d6a6092f59a
nourmattar2620-ai <nourmattar2620@gmail.com>
2026-09-08 03:52:29 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
  * Service Worker بسيط
  * يخزّن هيكل التطبيق (الواجهة) مؤقتاً ليعمل عند ضعف الاتصال
  */
-const CACHE_NAME = "bioatlas-shell-v1";
+const CACHE_NAME = "bioatlas-shell-v2";
 const SHELL_FILES = [
   "./",
   "./index.html",
